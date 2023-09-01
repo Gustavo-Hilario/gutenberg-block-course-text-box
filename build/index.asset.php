dcf9405db9dc2b50d6d647b7dd236683795131ac
Gustavo_H <gustavo.hilario@automattic.com>
2023-08-31 20:06:42 -0500

    35. Adding Support for Different Style Options

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8c8d9d41e68065c272dd');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'a390f52a73a15da8bad2');
